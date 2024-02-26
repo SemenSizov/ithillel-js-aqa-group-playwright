db362175267fe959330d0bc925c035f526a30342
Semen Syzov <ssyzov@deloitte.com>
2024-02-26 18:45:52 +0200

    upd

--- setup/data.cleanup.js
+++ setup/data.cleanup.js
@@ -4,7 +4,6 @@ const { CarsController } = require('../src/controllers/CarsController');
 test('Delete user cars', async ({ request }) => {
 	const carsController = new CarsController(request);
 	const cars = await carsController.getAllCars();
-	console.log('cars', cars.json.data);
 	const carIds = cars.json.data.map((car) => car.id);
 	for (const id of carIds) {
 		await carsController.deleteCar(id);
